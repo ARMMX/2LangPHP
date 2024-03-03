7e01633c72acc8e18e68bbfcaa0aaf99a550378a
ARMMX <armthitinan.se@gmail.com>
2024-03-03 21:17:53 +0700

    backup

---
+++ lang/en.php
@@ -0,0 +1,8 @@
+<?php
+$lang = array(
+    'title' => 'English',
+    'hello' => 'Welcome Back Worawaran!',
+    'partner' => 'partner of my team ',
+    'menu' => 'List Of Menu',
+    'promotion' => 'promotion 50% discount!!'
+);
